22f1da96c2dcc7ab6de5fe53d2cbbfa284867b58
rogierg <rogierg@hotmail.com>
2026-08-28 15:19:21 +0200

    Add dum dadumdum bass comment and breakdown marker to Breaking Ties

--- breakingties.js
+++ breakingties.js
@@ -3,8 +3,9 @@
 // - Starts with just a kick
 // - Then some kind of chord on the first and second beat
 // - Then a soft choir kind of sound comes in
-// - Then the typical dum dumdumdum trance bass
+// - Then the typical dum dadumdum trance bass
 // - And only then the arpeggio
+// CAN START BREAKDOWN FROM HERE
 
 // 1. LOAD PREBAKE SCRIPT (SwitchAngel helpers)
 await fetch('https://raw.githubusercontent.com/switchangel/strudel-scripts/refs/heads/main/prebake.strudel')
