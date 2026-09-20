b1be1d6321e7c5182df11ba07386fc473fe8e041
rogierg <rogierg@hotmail.com>
2026-09-20 15:49:37 +0200

    Modify note sequence and sound parameters in piano.js

--- piano.js
+++ piano.js
@@ -1,6 +1,6 @@
 setcpm(135 / 4)
 
-$: note("[~ g5 ~ ~ ~ g6 ~ ~ ~ g7 ~ ~ ~ d#7 ~ ~ ~ d7 ~ ~ ~ g6 ~ ~]").sound("piano").slow(2).delay(0.5).delaytime(1/3).delayfeedback(0.3).orbit(6)
+$: note("[~ ~ ~ g5 ~ ~ ~ g6 ~ ~ ~ g7 ~ ~ ~ d#7 ~ ~ ~ d7 ~ ~ ~ g6 ~ ~ ~ d7 ~ ~ ~ g6]").sound("piano").slow(4).delay(0.7).delaytime(1/3).delayfeedback(0.7).orbit(6)
 
 // STEP 1: KICK
 $: s("rolandtr909_bd:2!4").gain(.7)._scope().orbit(0)
