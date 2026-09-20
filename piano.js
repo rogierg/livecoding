6a6fa1f4566a18481556931324e72f02f6e420e6
rogierg <rogierg@hotmail.com>
2026-09-20 15:38:59 +0200

    Adjust tempo and enhance note sequence in piano.js
    Updated tempo and modified note sequence with delays and feedback.

--- piano.js
+++ piano.js
@@ -1,6 +1,6 @@
-setcpm(120 / 4)
+setcpm(135 / 4)
 
-$: note("[~ g5 ~ g6 ~ g7 ~ d#7 ~ d7]*2").sound("piano")
+$: note("[~ g5 ~ ~ ~ g6 ~ ~ ~ g7 ~ ~ ~ d#7 ~ ~ ~ d7 ~ ~ ~ g6 ~ ~]").sound("piano").slow(2).delay(0.5).delaytime(1/3).delayfeedback(0.3).orbit(6)
 
 // STEP 1: KICK
 $: s("rolandtr909_bd:2!4").gain(.7)._scope().orbit(0)
